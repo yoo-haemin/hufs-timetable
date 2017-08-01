2b7e780a7fde309428581a903e37a68dfcde2eb4
SeeYoungLee <sylec15@gmail.com>
2017-08-01 23:02:55 +0900

    0801

---
+++ public/timetable.js
@@ -0,0 +1,10 @@
+function closeSideNav() {
+    document.getElementById("side_nav").style.display = "none";
+}
+
+function openSideNav() {
+    document.getElementById("side_nav").style.display = "block";
+    document.getElementById("side_nav").style.height = "100%";
+    document.getElementById("side_nav").style.width = "340px";
+}
+
